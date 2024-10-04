78b4b8fe6cec22ececb7d4fe34ddc26444b7cb2f
Tanay <tanayrox2004@gmail.com>
2024-10-04 15:11:44 +0530

    added models

--- src/index.js
+++ src/index.js
@@ -9,23 +9,12 @@ dotenv.config({
   path: "./env",
 });
 
-conenctDB();
-
-/*
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.error("Error in server: ", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
+conenctDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-})();
-*/
+  })
+  .catch((error) => {
+    console.log("Mongo db connection error ", error);
+  });
